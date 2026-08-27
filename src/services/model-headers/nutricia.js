12781534e022c9838bcd584646aedfd20d7d6280
John Fox <163529520+JohnFoxCap@users.noreply.github.com>
2026-08-27 15:04:13 +0100

    Corrected net weight mapping (#173)

--- src/services/model-headers/nutricia.js
+++ src/services/model-headers/nutricia.js
@@ -14,7 +14,7 @@ const nutriciaHeaders = {
       description: /^Description$/i,
       commodity_code: /Commodity Code/i,
       number_of_packages: /Quantity/i,
-      total_net_weight_kg: /Net ?Weight \(KG\)/i
+      total_net_weight_kg: /Net ?Weight/i
     },
     country_of_origin: /Country of origin/i,
     blanketNatureOfProductsValue: {
